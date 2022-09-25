9ed24f6cc1f31ce500617794f6468976ba135254
gursajalsingh <gursajalsingh1113@gmail.com>
2022-09-25 16:26:02 +0530

    First commit

---
+++ medop/location map/Untitled-2.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Location of Pune Hospitals </title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="top">Links to Sections of The Same Page</h1>
+  <section>
+    <ul>
+      <!-- Link to every section in the page -->
+      <li><a href="#section1">#section1</a></li>
+      <li><a href="#section2">#section2</a></li>
+      <li><a href="#section3">#section3</a></li>
+      <li><a href="#section4">#section4</a></li>
+      <li><a href="#section5">#section5</a></li>
+      <li><a href="#section6">#section6</a></li>
+    </ul>
+
+  </section>
+    <div id="map"></div>
+    
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5lBaUSzJh25BjLBpJ5KetHvjQf3EYg_8&callback=initMap">
+    </script>
+  </body>
+</html>
